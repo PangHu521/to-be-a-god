febaa2e4fd7b0d093131c93265b95e5e5a83e91b
zy <2486899356@qq.com>
2026-05-19 21:49:08 +0800

    修改complie版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.deify.app"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.deify.app"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
     }
